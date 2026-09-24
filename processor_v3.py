4b0c958facff20ea7fde624b1bf320dbe690d559
Hernán Brito <hernanbrito@MacBook-Pro-2018-HJ.local>
2026-09-23 22:07:20 -0300

    Fix: Syntax error in processor_v3.py

--- processor_v3.py
+++ processor_v3.py
@@ -82,8 +82,7 @@ def extract_text_from_pdf(file, password=""):
             for page in pdf.pages:
                 page_text = page.extract_text()
                 if page_text:
-                    text += page_text + "
-"
+                    text += page_text + "\n"
         return text
         
     except Exception as e:
